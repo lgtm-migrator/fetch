b759beb96e8194cf102c20f44dcd7c00fd32f55d
equt <git@47.rs>
2021-09-27 20:41:20 +0800

    feat: Copy `FormData` logics from `t`

--- src/monad.ts
+++ src/monad.ts
@@ -6,6 +6,7 @@ import {
   chainTaskEitherK,
   chainTaskEitherKW,
   chainEitherKW,
+  local,
 } from 'fp-ts/ReaderTaskEither'
 import { foldW } from 'fp-ts/Option'
 import { pipe } from 'fp-ts/function'
@@ -139,3 +140,33 @@ export const withDecoder = <E extends Error, S extends z.ZodTypeAny>(
         )
     )
   )
+
+type FormBlob = {
+  blob: Blob
+  filename?: string
+}
+
+const isFormBlob = (x: unknown): x is FormBlob => !!(x as FormBlob).blob
+
+export type Formable = FormableK | FormableKV
+
+type FormableK = Record<string, string | Blob | FormBlob>
+
+type FormableKV = Record<
+  string,
+  string | Blob | FormBlob | { toString: () => string }
+>
+
+const mkFormData = (form: Formable | FormableK): FormData =>
+  Object.entries(form).reduce((m, [k, v]) => {
+    if (typeof v === 'string') {
+      m.set(k, v)
+    } else if (v instanceof Blob) {
+      m.set(k, v)
+    } else if (isFormBlob(v)) {
+      m.set(k, v.blob, v.filename)
+    } else {
+      m.set(k, v.toString())
+    }
+    return m
+  }, new FormData())
