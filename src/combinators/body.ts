40a5e4a5eb01f2a13298b4f634a3b71afa17d263
equt <git@47.rs>
2021-09-28 10:22:11 +0800

    feat: Add `withBlob` & `withArrayBuffer`

--- src/combinators/body.ts
+++ src/combinators/body.ts
@@ -70,3 +70,23 @@ export const withJSON = <E extends Error, A>(
     withJSONBody(json, replacer, space),
     withHeaders({ 'Content-Type': 'application/json' })
   )
+
+export const withBlob = <E extends Error, A>(blob: Blob): Combinator<E, A> =>
+  local(({ input, init }) => ({
+    input,
+    init: {
+      body: blob,
+      ...init,
+    },
+  }))
+
+export const withArrayBuffer = <E extends Error, A>(
+  buf: ArrayBuffer
+): Combinator<E, A> =>
+  local(({ input, init }) => ({
+    input,
+    init: {
+      body: buf,
+      ...init,
+    },
+  }))
