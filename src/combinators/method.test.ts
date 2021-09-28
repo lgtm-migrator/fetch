56c4c12bf99c364ea23331cbac9de7585156d808
equt <git@47.rs>
2021-09-28 14:45:24 +0800

    chore: Collect mock together

--- src/combinators/method.test.ts
+++ src/combinators/method.test.ts
@@ -3,36 +3,28 @@ import { request, config } from '../monad'
 import mock from 'fetch-mock-jest'
 import { pipe } from 'fp-ts/function'
 
+beforeEach(() => mock.mock('https://example.com', 200))
 afterEach(() => mock.reset())
 
 const mk = config('https://example.com')
 
 describe('Method combinator', () => {
   it('should set method correctly', async () => {
-    mock.mock('https://example.com', 200)
-
     await pipe(request, withMethod('POST'), mk)()
-
-    expect(mock.lastOptions()).toEqual({ method: 'POST' })
+    expect(mock.lastOptions()).toStrictEqual({ method: 'POST' })
   })
 
   it('latter combinator should take the precedence', async () => {
-    mock.mock('https://example.com', 200)
-
     await pipe(request, withMethod('POST'), withMethod('DELETE'), mk)()
-
-    expect(mock.lastOptions()).toEqual({ method: 'DELETE' })
+    expect(mock.lastOptions()).toStrictEqual({ method: 'DELETE' })
   })
 
   it('config should take the precedence', async () => {
-    mock.mock('https://example.com', 200)
-
     await pipe(
       request,
       withMethod('DELETE'),
       config('https://example.com', { method: 'GET' })
     )()
-
-    expect(mock.lastOptions()).toEqual({ method: 'GET' })
+    expect(mock.lastOptions()).toStrictEqual({ method: 'GET' })
   })
 })
