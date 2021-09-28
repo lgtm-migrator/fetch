94756bc9a2dd9bacb95b5728bbf701fe98aa8471
equt <git@47.rs>
2021-09-28 10:44:52 +0800

    feat: Add `withSignal`

---
+++ src/combinators/controller.ts
@@ -0,0 +1,7 @@
+import type { Combinator, Config } from '../monad'
+import { local } from 'fp-ts/ReaderTaskEither'
+
+export const withSignal = <E extends Error, A>(
+  signal: AbortSignal
+): Combinator<E, A, E | DOMException> =>
+  local(({ input, init }): Config => ({ input, init: { signal, ...init } }))
