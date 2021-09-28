63d2950f9a0f257de9c75eb15ed68043c8060e97
equt <git@47.rs>
2021-09-28 16:41:57 +0800

    test: Add test cases for `mkFormData`

--- src/combinators/body.test.ts
+++ src/combinators/body.test.ts
@@ -1,10 +1,48 @@
 import mock from 'fetch-mock-jest'
 import { pipe } from 'fp-ts/function'
 import { post, config } from '../monad'
-import { withForm, withJSON } from './body'
+import { withForm, withJSON, mkFormData } from './body'
 
 beforeEach(() => mock.mock('https://example.com', 200))
 afterEach(() => mock.reset())
+
+describe('Create FormData', () => {
+  it('build from strict Formable', () => {
+    const generated = mkFormData({
+      name: 'John',
+      license: new Blob(['DATA']),
+      backup: {
+        blob: new Blob(['DATA']),
+        filename: 'Backup',
+      },
+    })
+
+    const expected = new FormData()
+    expected.set('name', 'John')
+    expected.set('license', new Blob(['DATA']))
+    expected.set('backup', new Blob(['DATA']), 'Backup')
+
+    expect(generated).toStrictEqual(expected)
+  })
+
+  it('build from weak Formable', () => {
+    const generated = mkFormData({
+      id: 1,
+      license: new Blob(['DATA']),
+      backup: {
+        blob: new Blob(['DATA']),
+        filename: 'Backup',
+      },
+    })
+
+    const expected = new FormData()
+    expected.set('id', '1')
+    expected.set('license', new Blob(['DATA']))
+    expected.set('backup', new Blob(['DATA']), 'Backup')
+
+    expect(generated).toStrictEqual(expected)
+  })
+})
 
 const mk = config('https://example.com')
 
