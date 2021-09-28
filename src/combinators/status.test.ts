651cf2594ec81d09a03b629b2d07c7045bd73238
equt <git@47.rs>
2021-09-28 15:02:05 +0800

    test: Add test cases for status

---
+++ src/combinators/status.test.ts
@@ -0,0 +1,44 @@
+import { guard } from './status'
+import mock from 'fetch-mock-jest'
+import { config, request } from '../monad'
+import { pipe } from 'fp-ts/function'
+import { some, none } from 'fp-ts/Option'
+import { left } from 'fp-ts/Either'
+
+afterEach(() => mock.reset())
+
+const mk = config('https://example.com')
+
+describe('Status combinator', () => {
+  it('should return error if satisfied', async () => {
+    mock.mock('https://example.com', 400)
+
+    class CustomError extends Error {}
+
+    expect(
+      await pipe(
+        request,
+        guard(n =>
+          n >= 400 ? some(new CustomError('Always Has Been')) : none
+        ),
+        mk
+      )()
+    ).toStrictEqual(left(new CustomError('Always Has Been')))
+  })
+
+  it('should return none if not satisfied', async () => {
+    mock.mock('https://example.com', 200)
+
+    class CustomError extends Error {}
+
+    expect(
+      await pipe(
+        request,
+        guard(n =>
+          n >= 400 ? some(new CustomError('Always Has Been')) : none
+        ),
+        mk
+      )()
+    ).toStrictEqual(expect.objectContaining({ _tag: 'Right' }))
+  })
+})
