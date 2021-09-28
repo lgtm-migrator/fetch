a5ede1bed557a67c843cda24347f8b175dd9a052
equt <git@47.rs>
2021-09-28 11:31:52 +0800

    chore: Be polite

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,10 @@
 export const unreachable = (): never => {
-  throw new Error('WTF')
+  throw new Error('What!? How could you get there?')
+}
+
+export class NonExistError extends Error {
+  constructor(message: string) {
+    super(message)
+    throw new Error('What!? How could you ever create a non-existing error?')
+  }
 }
