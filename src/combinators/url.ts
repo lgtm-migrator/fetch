a57272865cfef19635e3e28824d717d524a72eb2
equt <git@47.rs>
2021-09-29 10:05:23 +0800

    feat: Allow empty base URL

--- src/combinators/url.ts
+++ src/combinators/url.ts
@@ -2,7 +2,7 @@ import { local } from 'fp-ts/ReaderTaskEither'
 import type { Combinator } from '..'
 
 export const withBaseURL = <E extends Error, A>(
-  url: URL | string
+  url?: URL | string
 ): Combinator<E, A> =>
   local(({ input, init }) => ({
     init,
