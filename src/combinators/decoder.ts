5ab7cbc7d91bb87daeef8248239319b2c26df517
equt <git@47.rs>
2021-09-29 13:19:33 +0800

    fix: Wrap zod error

--- src/combinators/decoder.ts
+++ src/combinators/decoder.ts
@@ -1,16 +1,22 @@
 import type { Combinator } from '..'
 import { left, right } from 'fp-ts/Either'
-import { chainTaskEitherKW } from 'fp-ts/ReaderTaskEither'
+import { chainTaskEitherK } from 'fp-ts/ReaderTaskEither'
 import { z } from 'zod'
+
+export type DecodeError = {
+  kind: 'ZodDecodeError'
+  error: z.ZodError
+}
 
 export const withDecoder = <E, S extends z.ZodTypeAny>(
   s: S,
   params?: Partial<z.ParseParamsNoData>
-): Combinator<E, unknown, z.ZodError, z.infer<S>> =>
-  chainTaskEitherKW(
+): Combinator<E, unknown, DecodeError, z.infer<S>> =>
+  chainTaskEitherK<E | DecodeError, unknown, z.infer<S>>(
     x => () =>
       s.parseAsync(x, params).then(
         x => right(x),
-        (e: unknown) => left(e as z.ZodError)
+        (error: unknown) =>
+          left({ kind: 'ZodDecodeError', error: error as z.ZodError })
       )
   )
