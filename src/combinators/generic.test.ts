71b35550d3406ce6bffb35c7e101601a70e0f6ed
equt <git@47.rs>
2021-10-02 16:59:13 +0800

    revert: Add back `withDecoder` test

---
+++ src/combinators/generic.test.ts
@@ -0,0 +1,45 @@
+import { withDecoder } from './generic'
+import mock from 'fetch-mock-jest'
+import { Response } from 'cross-fetch'
+import { pipe } from 'fp-ts/function'
+import { runFetchM, request } from '..'
+import { asJSON } from './parser'
+import { left, right } from 'fp-ts/Either'
+
+afterEach(() => mock.reset())
+
+const mk = runFetchM('https://example.com')
+
+describe('Decoder combinator', () => {
+  it('should decode', async () => {
+    mock.mock(
+      'https://example.com',
+      new Response(`{ "Earth": "Always Has Been" }`)
+    )
+
+    expect(
+      await pipe(
+        request,
+        asJSON(),
+        withDecoder(() => right({})),
+        mk
+      )()
+    ).toStrictEqual(right({}))
+  })
+
+  it('throws if invalid', async () => {
+    mock.mock(
+      'https://example.com',
+      new Response(`{ "Earth": "Always Has Been" }`)
+    )
+
+    expect(
+      await pipe(
+        request,
+        asJSON(),
+        withDecoder(() => left({})),
+        mk
+      )()
+    ).toStrictEqual(left({}))
+  })
+})
