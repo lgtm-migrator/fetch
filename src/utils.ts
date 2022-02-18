08ed992747a39b83285479b142c0983cc029bf89
equt <17521736+equt@users.noreply.github.com>
2022-02-18 14:43:42 +0800

    docs: Add doc for `eager`

--- src/utils.ts
+++ src/utils.ts
@@ -2,4 +2,11 @@ import type { Lazy } from 'fp-ts/function'
 
 const isLazy = <A>(a: A | Lazy<A>): a is Lazy<A> => typeof a === 'function'
 
+/**
+ * Require the expression to be reduced.
+ *
+ * @param lazy A value that might be lazy
+ *
+ * @since 2.3.0
+ */
 export const eager = <A>(lazy: Lazy<A> | A): A => (isLazy(lazy) ? lazy() : lazy)
